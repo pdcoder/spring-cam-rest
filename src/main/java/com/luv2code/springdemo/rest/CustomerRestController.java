cfa6006e2a78d7c4a4210c220ac76afa24e1b7d6
Prakash <prakashdas1998.pd@gmail.com>
2018-08-14 23:44:52 +0530

    Added files

---
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -0,0 +1,35 @@
+package com.luv2code.springdemo.rest;
+
+import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.GeneratedValue;
+import java.util.List;
+
+/**
+ * Created by prakashdas on 14/08/18.
+ */
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/customers")
+    public List<Customer> getCustomers(){
+        return customerService.getCustomers();
+    }
+
+    @GetMapping("/customers/{customerId}")
+    public Customer getCustomer(@PathVariable int customerid){
+        Customer theCustomer = customerService.getCustomer(customerid);
+                return theCustomer;
+    }
+}
