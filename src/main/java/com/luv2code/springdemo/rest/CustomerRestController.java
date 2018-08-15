71cea351addb1af568d1d3bfdc5ed423d84eb10c
Prakash <prakashdas1998.pd@gmail.com>
2018-08-15 17:44:59 +0530

    Added rest security

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -1,60 +1,85 @@
 package com.luv2code.springdemo.rest;
 
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.rest.CustomNotFoundException;
 import com.luv2code.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.GeneratedValue;
 import java.util.List;
-
-/**
- * Created by prakashdas on 14/08/18.
- */
 
 @RestController
 @RequestMapping("/api")
 public class CustomerRestController {
 
+    // autowire the CustomerService
     @Autowired
     private CustomerService customerService;
 
+    // add mapping for GET /customers
     @GetMapping("/customers")
-    public List<Customer> getCustomers(){
+    public List<Customer> getCustomers() {
+
         return customerService.getCustomers();
+
     }
+
+    // add mapping for GET /customers/{customerId}
 
     @GetMapping("/customers/{customerId}")
-    public Customer getCustomer(@PathVariable int customerid){
-        Customer theCustomer = customerService.getCustomer(customerid);
+    public Customer getCustomer(@PathVariable int customerId) {
 
-               if(theCustomer==null){
-                   throw  new CustomNotFoundException(customerid);
-               }
-                return theCustomer;
+        Customer theCustomer = customerService.getCustomer(customerId);
+
+        if (theCustomer == null) {
+            throw new CustomNotFoundException("Customer id not found - " + customerId);
+        }
+
+        return theCustomer;
     }
+
+    // add mapping for POST /customers  - add new customer
 
     @PostMapping("/customers")
-    public Customer addCustomer(@RequestBody Customer theCustomer){
+    public Customer addCustomer(@RequestBody Customer theCustomer) {
+
+        // also just in case the pass an id in JSON ... set id to 0
+        // this is force a save of new item ... instead of update
+
         theCustomer.setId(0);
+
         customerService.saveCustomer(theCustomer);
+
         return theCustomer;
     }
+
+    // add mapping for PUT /customers - update existing customer
 
     @PutMapping("/customers")
-    public Customer updateCustomer(@PathVariable Customer theCustomer){
+    public Customer updateCustomer(@RequestBody Customer theCustomer) {
+
         customerService.saveCustomer(theCustomer);
+
         return theCustomer;
+
     }
+
+    // add mapping for DELETE /customers/{customerId} - delete customer
 
     @DeleteMapping("/customers/{customerId}")
-    public String deleteCustomer(@PathVariable int customerId){
+    public String deleteCustomer(@PathVariable int customerId) {
 
         Customer tempCustomer = customerService.getCustomer(customerId);
-        if(tempCustomer==null){
-            throw new CustomNotFoundException("Customer not fouend");
+
+        // throw exception if null
+
+        if (tempCustomer == null) {
+            throw new CustomNotFoundException("Customer id not found - " + customerId);
         }
+
         customerService.deleteCustomer(customerId);
-        return "The id that was deleted was - " + customerId;
+
+        return "Deleted customer id - " + customerId;
     }
+
 }
